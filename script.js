388aa0c1ebb18dea4364a2da1cbad77edb1d06d6
isak <isakmpederson@gmail.com>
2025-09-24 15:19:19 -0700

    Add hover effect using event listener and implementing random color function

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 const container = document.getElementById("container");
 
+function getRandomRgbColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
 const createGrid = () => {
     for (let i = 0; i < 16 * 16; i++)
     {
         const box = document.createElement("div");
         box.classList.add('box');
-        box.style.width = "45px";
-        box.style.height = "45px";
+        box.style.width = "40px";
+        box.style.height = "40px";
         container.appendChild(box);
+
+        box.addEventListener('mouseenter', () => {
+            box.style.backgroundColor = getRandomRgbColor();
+        })
     }
 }
 
